3b57984ea630d489fcaa87ac3d28a9542dcf6f73
kebinRamirez <kebinr@uninorte.edu.co>
2022-11-10 06:13:37 -0500

    some updates

--- src/components/EmptyComponent.tsx
+++ src/components/EmptyComponent.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
